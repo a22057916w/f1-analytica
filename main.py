37d0cb176d9a28ca9ec7ded9a3e17d638a5e3f14
naitea9527 <a22057916w@gmail.com>
2026-02-23 17:21:20 +0800

    add corner number

--- main.py
+++ main.py
@@ -30,6 +30,29 @@ def main():
     # Rotate and plot the track map.
     rotated_track = rotate(track, angle=track_angle)
     plt.plot(rotated_track[:, 0], rotated_track[:, 1])
+
+    offset_vector = [500, 0]
+    for _, corner in circuit_info.corners.iterrows():
+        txt = f"{corner['Number']}{corner['Letter']}"
+
+        offset_angle = corner["Angle"] / 180 * np.pi
+        offset_x, offset_y = rotate(offset_vector, angle=offset_angle)
+
+        text_x = corner['X'] + offset_x
+        text_y = corner['Y'] + offset_y
+
+        text_x, text_y = rotate([text_x, text_y], angle=track_angle)
+
+        track_x, track_y = rotate([corner['X'], corner['Y']], angle=track_angle)
+
+        plt.scatter(text_x, text_y, color='grey', s=140)
+        plt.plot([track_x, text_x], [track_y, text_y], color='grey')
+        plt.text(text_x, text_y, txt, va='center_baseline', ha='center', size='small', color='white')
+    
+    plt.title(session.event['Location'])
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis('equal')
     plt.show()
 
 if __name__ == '__main__':
